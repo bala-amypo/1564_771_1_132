c11282b639dc43830313b23ad8e70d50ea8990da
bala-amypo <bala@amypo.in>
2025-12-16 14:24:29 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,23 @@
 package com.example.Barter.model;
 
 public class User{
-    private 
+    private long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String role;
+    private LocalDateTime createdAt;
+
+    public User(){
+
+    }
+
+    public User(long id,String fullName,String email,String password,String role,LocalDateTime createdAt){
+        this.fullName=fullName;
+        this.email=email;
+        this.password=password;
+        this.role=role;
+        this.createdAt=createdAt;
+    }
+    
 }
