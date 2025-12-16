b20ae3251e654202932ac42331ada38072ecb1ec
bala-amypo <bala@amypo.in>
2025-12-16 10:20:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Skill.java
@@ -0,0 +1,67 @@
+package com.example.Barter.model;
+
+public class Skill {
+    private long id;
+    private String name;
+    private String category;
+    private String description;
+    private boolean active;
+
+    public Skill(){
+
+    }
+
+    public Skill(long id, String name, String category, String description, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
+
+
+
+
+}
+
