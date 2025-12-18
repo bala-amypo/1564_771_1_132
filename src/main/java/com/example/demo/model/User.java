32ef72f2c86fba2750b0bcfd1adb331aad941b29
bala-amypo <bala@amypo.in>
2025-12-18 06:18:15 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,66 +1,87 @@
 package com.example.demo.model;
-@Entity
+
 import java.time.LocalDateTime;
 
-public class User{
-    private long id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String fullName;
     private String email;
     private String password;
     private String role;
     private LocalDateTime createdAt;
 
-    public User(){
-
-    }
-
-    public User(long id,String fullName,String email,String password,String role,LocalDateTime createdAt){
-        this.fullName=fullName;
-        this.email=email;
-        this.password=password;
-        this.role=role;
-        this.createdAt=createdAt;
-    }
-    public long getId() {
-        return id;
-    }
-    public String fullName() {
-        return fullName;
-    }
-    public String email() {
-        return email;
-    }
-    public String password() {
-        return password;
-    }
-    public String role() {
-        return role;
-    }
-    public LocalDateTime createdAt() {
-        return createdAt;
-    }
     
-    public void setId(long id) {
+    public User() {
+    }
+
+   
+    public User(Long id, String fullName, String email, String password,
+                String role, LocalDateTime createdAt) {
         this.id = id;
-    }
-    public void setfullName(String fullName) {
-        this.fullName=fullName;
-    }
-    public void setemail(String email ) {
-        this.email=email;
-    }
-    public void setpassword(String password) {
-        this.password=password;
-    }
-    public void serrole(String role) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
         this.role = role;
-    }
-    public void setcreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
-    
-    
-    
+
+   
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
-
-
