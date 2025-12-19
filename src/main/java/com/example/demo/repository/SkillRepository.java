7a8ebbc8eebbf7642fad9027f57e48c77641cbd0
bala-amypo <bala@amypo.in>
2025-12-19 09:45:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillRepository.java
+++ src/main/java/com/example/demo/repository/SkillRepository.java
@@ -1,11 +1,11 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import com.example.demo.model.Skill;
+import com.example.demo.model.Skill;
 
-// import java.util.Optional;
+import java.util.Optional;
 
-// public interface SkillRepository extends JpaRepository<Skill, Long> {
-//     Optional<Skill> findByName(String name);
-// }
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+    Optional<Skill> findByName(String name);
+}
--- src/main/java/com/example/demo/repository/SkillRequestRepository.java
+++ src/main/java/com/example/demo/repository/SkillRequestRepository.java
@@ -1,11 +1,11 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import com.example.demo.model.SkillRequest;
+import com.example.demo.model.SkillRequest;
 
-// import java.util.List;
+import java.util.List;
 
-// public interface SkillRequestRepository extends JpaRepository<SkillRequest, Long> {
-//     List<SkillRequest> findByUser_Id(Long userId);
-// }
+public interface SkillRequestRepository extends JpaRepository<SkillRequest, Long> {
+    List<SkillRequest> findByUser_Id(Long userId);
+}
