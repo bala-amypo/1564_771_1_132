3686e3f00b4e49586c1f7581e59d1565c876f1cb
bala-amypo <bala@amypo.in>
2025-12-25 06:57:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,64 @@
+package com.example.barter.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+    
+    private final String secret;
+    private final long validityInMs;
+    
+    public JwtUtil(@Value("${jwt.secret}") String secret, 
+                   @Value("${jwt.validity}") long validityInMs) {
+        this.secret = secret;
+        this.validityInMs = validityInMs;
+    }
+    
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+    
+    public String generateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + validityInMs))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    
+    public Map<String, Object> getAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+    
+    public String getEmail(String token) {
+        return (String) getAllClaims(token).get("email");
+    }
+    
+    public String getRole(String token) {
+        return (String) getAllClaims(token).get("role");
+    }
+}
