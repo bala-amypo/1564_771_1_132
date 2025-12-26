65c65db25c618e2504b969720376ea3be2243f9f
bala-amypo <bala@amypo.in>
2025-12-26 16:37:52 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -28,10 +28,33 @@
 // }
 
 
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.info.Info;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// public class OpenApiConfig {
+
+//     @Bean
+//     public OpenAPI openAPI() {
+//         return new OpenAPI()
+//                 .info(new Info()
+//                         .title("Skill Barter Matchmaking Platform API")
+//                         .version("1.0")
+//                         .description("API documentation"));
+//     }
+// }
+
+
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -44,6 +67,14 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Skill Barter Matchmaking Platform API")
                         .version("1.0")
-                        .description("API documentation"));
+                        .description("API documentation"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
     }
 }
+
